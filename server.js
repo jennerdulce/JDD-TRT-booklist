6c5e6bca83524e6704354867e8a77b813336bc9c
Jenner Dulce <jennerdulce@gmail.com>
2021-01-18 18:04:33 -0800

    added css

--- server.js
+++ server.js
@@ -6,57 +6,74 @@ const superagent = require('superagent');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
-app.use(express.urlencoded({extended: true}));
+// for post
+// change .get => .post
+// change method="GET" => method="POST"
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', defaultHandler);
-app.get('/searches', searchHandler);
-app.get('/books', searchHandler);
+app.get('/searches', searchesHandler);
+app.get('/newSearches', newSearchHandler);
 
 
 
-function defaultHandler(req, res){
-  res.status(200).render('index');
+function defaultHandler(req, res) {
+  res.status(200).render('pages/index')
+    .catch(() => {
+      handleError(res);
+    });
 }
 
-function searchHandler(req, res){
+function searchesHandler(req, res) {
+  res.status(200).render('pages/searches/show')
+    .catch(() => {
+      handleError(res);
+    });
+}
+
+function newSearchHandler(req, res) {
   console.log('req.query >>>>>>>>> ', req.query);
 
   let url = `https://www.googleapis.com/books/v1/volumes?q=`;
-  if (req.query.search[1] === 'title'){
-    url += `+intitle:${req.query.search[1]}`;
+  if (req.query.keyword === 'title') {
+    url += `+intitle:${req.query.search}`;
   }
 
-  if (req.query.search[1] === 'author'){
-    url += `+inauthor:${req.query.search[1]}`;
+  if (req.query.keyword === 'author') {
+    url += `+inauthor:${req.query.search}`;
   }
 
+  console.log('==========================', url);
   superagent.get(url)
     .then(value => {
-      console.log('value.body >>>>>>>>> ', value.body.items[0]);
+      console.log('value.body >>>>>>>>> ', value.body.items);
       const bookData = value.body.items;
       const books = bookData.map(value => {
         return new Book(value);
       });
       console.log('we are in the superagent ============');
-      res.status(200).render('books', {data: books});
+      res.status(200).render('pages/books', { data: books });
+    })
+    .catch(() => {
+      handleError(res);
     });
 }
 
-let placeholder = `https://i.imgur.com/J5LVHEL.jpg`;
-
-function Book(data){
-  this.title = data.volumeInfo.title;
-  this.author = data.volumeInfo.authors;
-  this.description = data.volumeInfo.description;
-  // this.thumbnail = data.volumeInfo.imageLinks.thumbnail || null;
+function handleError(res){
+  return res.status(500).render('pages/error');
 }
 
 
 
+let placeholder = `https://i.imgur.com/J5LVHEL.jpg`;
 
-
-
+function Book(data) {
+  this.title = data.volumeInfo.title;
+  this.author = data.volumeInfo.authors;
+  this.description = data.volumeInfo.description || '*** Description current unavailable ***';
+  this.thumbnail = data.volumeInfo.imageLinks.thumbnail || null;
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
